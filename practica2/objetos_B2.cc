7c2d86b638085c6d22ea5d46f73672141db817c3
José Miguel <jmiguelmh3000@gmail.com>
2021-10-29 17:44:05 +0200

    Práctica 2
    Fix en las caras del cubo

--- practica2/objetos_B2.cc
+++ practica2/objetos_B2.cc
@@ -135,22 +135,73 @@ void _triangulos3D::draw(_modo modo, float r1, float g1, float b1, float r2, flo
 
 _cubo::_cubo(float tam)
 {
-	vector<_vertex3f> perfil;
-	_vertex3f punto;
-	_rotacion cubo;
+  //vertices
+  vertices.resize(8);
 
-	punto.x = tam / 2;
-	punto.y = -tam / 2;
-	punto.z = -tam / 2;
-	perfil.push_back(punto);
-	punto.x = tam / 2;
-	punto.y = tam / 2;
-	punto.z = -tam / 2;
-	perfil.push_back(punto);
+  vertices[0].x = -tam;
+  vertices[0].y = 0;
+  vertices[0].z = tam;
+  vertices[1].x = tam;
+  vertices[1].y = 0;
+  vertices[1].z = tam;
+  vertices[2].x = tam;
+  vertices[2].y = 0;
+  vertices[2].z = -tam;
+  vertices[3].x = -tam;
+  vertices[3].y = 0;
+  vertices[3].z = -tam;
+  vertices[4].x = -tam;
+  vertices[4].y = 2 * tam;
+  vertices[4].z = tam;
+  vertices[5].x = tam;
+  vertices[5].y = 2 * tam;
+  vertices[5].z = tam;
+  vertices[6].x = tam;
+  vertices[6].y = 2 * tam;
+  vertices[6].z = -tam;
+  vertices[7].x = -tam;
+  vertices[7].y = 2 * tam;
+  vertices[7].z = -tam;
 
-	cubo.parametros(perfil, 4, 'y');
-	vertices = cubo.vertices;
-	caras = cubo.caras;
+  // triangulos
+  caras.resize(12);
+
+  caras[0]._0 = 0;
+  caras[0]._1 = 1;
+  caras[0]._2 = 5;
+  caras[1]._0 = 0;
+  caras[1]._1 = 4;
+  caras[1]._2 = 5;
+  caras[2]._0 = 1;
+  caras[2]._1 = 2;
+  caras[2]._2 = 6;
+  caras[3]._0 = 1;
+  caras[3]._1 = 5;
+  caras[3]._2 = 6;
+  caras[4]._0 = 2;
+  caras[4]._1 = 3;
+  caras[4]._2 = 7;
+  caras[5]._0 = 2;
+  caras[5]._1 = 6;
+  caras[5]._2 = 7;
+  caras[6]._0 = 3;
+  caras[6]._1 = 0;
+  caras[6]._2 = 4;
+  caras[7]._0 = 3;
+  caras[7]._1 = 4;
+  caras[7]._2 = 7;
+  caras[8]._0 = 0;
+  caras[8]._1 = 1;
+  caras[8]._2 = 3;
+  caras[9]._0 = 1;
+  caras[9]._1 = 2;
+  caras[9]._2 = 3;
+  caras[10]._0 = 4;
+  caras[10]._1 = 5;
+  caras[10]._2 = 6;
+  caras[11]._0 = 4;
+  caras[11]._1 = 6;
+  caras[11]._2 = 7;
 }
 
 //*************************************************************************
